703b7b5d07b9369968d8797b7fd3dbb99c074eff
ViktorDerkachov <97441771+ViktorDerkachov@users.noreply.github.com>
2022-04-12 18:53:08 +0300

    Update auth-provider-config.ts

--- config/custom/auth-provider-config.ts
+++ config/custom/auth-provider-config.ts
@@ -1,3 +1,17 @@
 import type { Nullable, AuthProviderConfig } from '../../types';
 
 export const authProviderConfig: Nullable<AuthProviderConfig> = null;
+
+import type { Nullable, AuthProviderConfig } from '../../types';
+import SlackProvider from 'next-auth/providers/slack';
+
+const SLACK_CLIENT_ID = process.env.SLACK_CLIENT_ID!;
+const SLACK_CLIENT_SECRET = process.env.SLACK_CLIENT_SECRET!;
+
+export const authProviderConfig: Nullable<AuthProviderConfig> = {
+  provider: SlackProvider({
+    clientId: SLACK_CLIENT_ID,
+    clientSecret: SLACK_CLIENT_SECRET,
+  }),
+  type: 'slack',
+};
